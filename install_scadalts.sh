e2bae35dcd2479669efaee07fa709cdd61f470e7
carl1961 <cmshelton2010@gmail.com>
2021-08-03 06:44:19 -0500

    updated

--- install_scadalts.sh
+++ install_scadalts.sh
@@ -4,25 +4,21 @@
 echo "Welcome to ScadaLTS installer!"
 
 apt-get update
-
-
-echo "   * Creating Install Folder..."
-mkdir -p  /opt/ScadaLTS 
-
-cp   ScadaBR.war /opt/ScadaLTS 
-cp   env.properties /opt/ScadaLTS 
-
-cd
-
 sudo apt-get install software-properties-common dirmngr
 sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+mkdir -p /opt/scadalts
+cp ScadaBR.war /opt/scadalts
+cp env.properties /opt/scadalts
 echo "Installing Tomcat"
+apt install -y tomcat
 
-apt  install -y tomcat9  
 echo "Installing Default Jdk"
 
 #install java 11
 apt install -y default-jdk
+
+
+ 
 echo "Install MariaDB Server"
 
 # MySQL/MariaDB
@@ -35,7 +31,9 @@ apt install -y librxtx-java
 
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
+
 root_password=admin
+ 
 # Make sure that NOBODY can access the server without a password
 
 sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
@@ -57,25 +55,28 @@ sudo mysql -e "flush privileges"
 
 echo "Installing Scada-LTS "
 
-echo " Extracting ScadaBR into Tomcat"
+
 mkdir -p /var/lib/tomcat9/webapps/ScadaBR
-cd   /opt/ScadaLTS 
-unzip ScadaBR.war -d /var/lib/tomcat9/webapps/ScadaBR
-
+cd /opt/scadalts
+unzip ScadaBR.war -d var/lib/tomcat9/webapps/ScadaBR
 sudo rm /var/lib/tomcat9/webapps/ScadaBR/WEB-INF/classes/env.properties
+cp env.properties /var/lib/tomcat9/webapps/ScadaBR/WEB-INF/classes/
 
-sudo cp env.properties /var/lib/tomcat9/webapps/ScadaBR/WEB-INF/classes/
+echo "Starting Scada-LTS "
+cd
 
-echo "Starting Tomcat9"
-sudo /var/lib/tomcat9/bin/startup.sh
+/var/lib/tomcat9/bin/startup.sh
 
-#echo "Cleaning Up!"
-cd 
-#echo "Removing Install Folder"
-sudo rm -rf  /opt/ScadaLTS  
+
+
+echo "Removing Install Folder"
+ 
+sudo rm -rf /opt/scadalts
+
 #echo "Removing ScadaLTS-Test Folder"
 
 sudo rm -rf ScadaLTS-Test
+
 echo "ScadaLTS Install Complete!"
 
 
