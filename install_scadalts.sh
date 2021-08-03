fc573e25aaa19ef7c316ebb4eb6fdf1ef4f31a0e
carl1961 <cmshelton2021@protonmail.com>
2021-08-02 23:41:24 -0500

    Update install_scadalts.sh

--- install_scadalts.sh
+++ install_scadalts.sh
@@ -2,33 +2,25 @@
 #email: cmshelton2021@protonmail.com
 # date 7/29/2021
 echo "Welcome to ScadaLTS installer!"
-sleep 2
+
 apt-get update
 echo "Installing Tomcat"
-sleep 2
 
-apt install -y tomcat9 
-
+apt-get install tomcat9  -y
 echo "Installing Default Jdk"
-sleep 2
+
 #install java 11
-apt install -y default-jdk
- 
+apt-get install default-jdk -y
 echo "Install MariaDB Server"
-sleep 2
+
 # MySQL/MariaDB
-apt install -y mariadb-server
-
+apt-get install -y mariadb-server
 # MySQL connector
-apt install -y libmariadb-java
-
-apt install -y librxtx-java
-
+apt-get install  libmariadb-java -y
+apt-get install  librxtx-java -y
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
-
 root_password=admin
- 
 # Make sure that NOBODY can access the server without a password
 
 sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
@@ -41,7 +33,7 @@ sudo mysql -e "DROP USER IF EXISTS ''@'$(hostname)'"
 sudo mysql -e "DROP DATABASE IF EXISTS test"
  
 echo "Creating scadalts database..."
-sleep 2 
+
 sudo mysql -e "create user 'scadalts' identified by 'scadalts'"
 sudo mysql -e "create database if not exists scadalts"
 sudo mysql -e "grant all privileges on scadalts.* to 'scadalts'"
@@ -49,15 +41,13 @@ sudo mysql -e "flush privileges"
 
 
 echo "Installing Scada-LTC "
-sleep 2
+
 
 mkdir -p /var/lib/tomcat9/webapps/ScadaBR
 
 unzip ScadaBR.war -d //var/lib/tomcat9/webapps/ScadaBR/webapps/ScadaBR
 sudo rm //var/lib/tomcat9/webapps/ScadaBR/webapps/ScadaBR/WEB-INF/classes/env.properties
 cp env.properties //var/lib/tomcat9/webapps/ScadaBR/webapps/ScadaBR/WEB-INF/classes/
-
-sleep 2
 
 
 echo "Removing install folder"
